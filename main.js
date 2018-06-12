69df426f221d4e681f4f7118d86f01e0d692f7dd
Unknown <masotoma@hotmail.com>
2018-06-12 14:20:55 +0200

    done

--- main.js
+++ main.js
@@ -1,42 +1,25 @@
-var x = window.matchMedia("(min-width: 600px)");
-var y = window.matchMedia("(max-width: 300px)");
-var z = window.matchMedia("(min-width: 1200px)");
 function createJsonFeed(resolution) {
   json = {
     get: 'user',
-    userId: '21006150',
-    clientId: '7632d49c1e574c8dbc295e75e5313962',
-    accessToken: '21006150.1677ed0.fbbf233a65d34bc1a0ace57fb6c31aa9',
+    userId: '2956328096',
+    clientId: '252adee577c54290b95bce5e0402d743',
+    accessToken: '2956328096.252adee.1236d11b36674de19e2208bfb4f9a4fe',
     resolution: resolution,
     limit: 4,
     template: '<a href="{{link}}">' +
       '<div class="post-container">' +
-      '<img class="high-res-img" src="{{image}}" />' +
+      '<img class="' + resolution + '" src="{{image}}" />' +
       '</div>' +
       '</a>'
-  }
-  return json
+  };
+  console.log(json);
+  return json;
 }
-
-
-
-
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener fu
-
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    var json = createJsonFeed('thumbnail');
-    var feed = new Instafeed(json);
-  } if (y.matches) {
-    var json = createJsonFeed('thumbnail');
-    var feed = new Instafeed(json);
-  } if (z.matches) {
-    var json = createJsonFeed('thumbnail');
-    var feed = new Instafeed(json);
-  }
-  else {
-    console.log("error");
-  }
+// render feed
+function renderFeed(resolution) {
+  var json = createJsonFeed(resolution);
+  var feed = new Instafeed(json);
+  console.log(feed);
   feed.run();
 }
+renderFeed('standard_resolution');
